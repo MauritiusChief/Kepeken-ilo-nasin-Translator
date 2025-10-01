cdae2b68ebc5199d8c5b0d48d10ed0e812088d2b
MauritiusChief <107936497+MauritiusChief@users.noreply.github.com>
2025-10-01 15:36:47 -0400

    叶子为toki pona单词的三级结构树

--- use_llm.js
+++ use_llm.js
@@ -269,6 +269,7 @@ export function useLLM() {
     console.log('clausesList: ', clausesList)
 
     // 解析从句，把从句内部的名词短语/动词短语也提取出来
+    let parsedClauses = [] // 盛装解析出来的结构化的从句
     try {
       // 创建从句解析承诺
       const parsePromieses = clausesList.map(clauseObj =>
@@ -276,7 +277,7 @@ export function useLLM() {
       )
       const settlements = await Promise.allSettled(parsePromieses)
       // 从句解析结果
-      const parsedClauses = settlements.map((settlement, index) => {
+      parsedClauses = settlements.map((settlement, index) => {
         if (settlement.status === 'fulfilled') {
           try {
             // 解析JSON字符串为对象
@@ -334,7 +335,7 @@ export function useLLM() {
 
     // const phrases = [{"id":"s0_subj","type":"主语","kind":"名词短语","text":""},{"id":"s0_pred#0","type":"谓语","kind":"动词短语","text":"轮回"},{"id":"s0_obj#0#0","type":"宾语","kind":"名词短语","text":"百转"},{"id":"s0_pp#0#0::subj","type":"主语","kind":"名词短语","text":""},{"id":"s0_pp#0#0::pred#0","type":"谓语","kind":"动词短语","text":"续"},{"id":"s0_pp#0#0::obj#0#0","type":"宾语","kind":"名词短语","text":"前缘"},{"id":"s0_pp#0#0::pp#0#0","type":"介词宾语","kind":"名词短语","text":"你"}]
 
-    const phrasesMap = new Map();
+    const phrasesMap = new Map(); // 盛装toki pona翻译过的，所有主句与从句的短语结构
     // 把结构化tokipona词组Object转化为字符串
     function tokiponaStringBuilder(obj) {
       function _nounPhrase(obj) {
@@ -362,24 +363,117 @@ export function useLLM() {
       const parsed = JSON.parse(content);
       // const parsed = {"results":[{"id":"s0_subj","parsed":[]},{"id":"s0_pred#0","parsed":{"前动词":"","动词":"kama","修饰词":["sin"]}},{"id":"s0_obj#0#0","parsed":[{"头词":"sike","修饰词":["mute"],"复合修饰词":[]}]},{"id":"s0_pp#0#0::subj","parsed":[]},{"id":"s0_pp#0#0::pred#0","parsed":{"前动词":"","动词":"awen","修饰词":[]}},{"id":"s0_pp#0#0::obj#0#0","parsed":[{"头词":"poka","修饰词":[],"复合修饰词":[{"头词":"tenpo","修饰词":["pini"]}]}]},{"id":"s0_pp#0#0::pp#0#0","parsed":[{"头词":"sina","修饰词":[],"复合修饰词":[]}]}]}
       for (const r of (parsed.results || [])) {
-        r.parsed = tokiponaStringBuilder(r.parsed)
+        // r.parsed = tokiponaStringBuilder(r.parsed)
         if (r && r.id) phrasesMap.set(r.id, r);
       }
+
+      // 把生成的toki pona结构回插到从句中
+      const parsedClausesMap = new Map() // id与其余成分分离的parsedClauses（详见 promptParseClause 中的结构）
+      parsedClauses.forEach(parsedClause => {
+        let baseId = parsedClause.id // 本从句所在成分的id
+        let parsedClauseObj = {} // 将被存储到map中的结构
+        // 情景列，插回情景
+        let contextList = parsedClause["情景列"]
+        parsedClauseObj["情景列"] = contextList.map((ctx, i) => {
+          let id = `${baseId}::ctx#${i}`
+          return phrasesMap.has(id) ? phrasesMap.get(id).parsed : ctx
+        })
+
+        // 主语
+        parsedClauseObj["主语"] = phrasesMap.get(`${baseId}::subj`).parsed
+
+        // 谓语列
+        if (parsedClause["谓语列"]) {
+          let vpList = parsedClause["谓语列"] || [];
+          parsedClauseObj["谓语列"] = vpList.map((vp, i) => {
+            // 插回谓语
+            if (vp["谓语"]) {
+              vp["谓语"] = phrasesMap.get(`${baseId}::pred#${i}`).parsed
+            }
+            // 宾语列，插回宾语
+            if (vp["宾语列"]) {
+              let objList = vp["宾语列"]
+              vp["宾语列"] = objList.map((obj, j) => {
+                let id = `${baseId}::obj#${i}#${j}`;
+                return phrasesMap.has(id) ? phrasesMap.get(id).parsed : obj;
+              })
+            }
+            // 介词短语列、介词短语，插回介词宾语
+            if (vp["介词短语列"]) {
+              vp["介词短语列"].forEach((pp, j) => {
+                pp["介词宾语"] = phrasesMap.get(`${baseId}::pp#${i}#${j}`).parsed;
+              })
+            }
+            return vp
+          })
+        }
+
+        parsedClausesMap.set(baseId, parsedClauseObj)
+      })
+      console.log('parsedClausesMap: ', parsedClausesMap)
+
+      // 把生成的toki pona结构与从句回插到原位，作为三级结构树
+      jsonTreeObj.forEach((sentenceObj, s) => {
+        // 情景列，插回情景
+        let contextList = sentenceObj["情景列"]
+        sentenceObj["情景列"] = contextList.map((ctx, i) => {
+          let id = `s${s}_ctx#${i}`
+          if (phrasesMap.has(id)) return phrasesMap.get(id).parsed
+          if (parsedClausesMap.has(id)) return parsedClausesMap.get(id)
+          return ctx
+        })
+
+        // 主语，主语不存在从句的情况
+        sentenceObj["主语"] = phrasesMap.get(`s${s}_subj`).parsed
+
+        // 谓语列
+        if (sentenceObj["谓语列"]) {
+          let vpList = sentenceObj["谓语列"] || [];
+          sentenceObj["谓语列"] = vpList.map((vp, i) => {
+            // 插回谓语，谓语不存在从句的情况
+            if (vp["谓语"]) {
+              vp["谓语"] = phrasesMap.get(`s${s}_pred#${i}`).parsed
+            }
+            // 宾语列，插回宾语
+            if (vp["宾语列"]) {
+              let objList = vp["宾语列"]
+              vp["宾语列"] = objList.map((obj, j) => {
+                let id = `s${s}_obj#${i}#${j}`;
+                if (phrasesMap.has(id)) return phrasesMap.get(id).parsed
+                if (parsedClausesMap.has(id)) return parsedClausesMap.get(id)
+                return obj
+              })
+            }
+            // 介词短语列、介词短语，插回介词宾语
+            if (vp["介词短语列"]) {
+              vp["介词短语列"].forEach((pp, j) => {
+                let ppId = `s${s}_pp#${i}#${j}`
+                if (phrasesMap.has(ppId)) pp["介词宾语"] = phrasesMap.get(ppId).parsed;
+                if (parsedClausesMap.has(ppId)) pp["介词宾语"] = parsedClausesMap.get(ppId);
+              })
+            }
+            return vp
+          })
+        }
+      });
+
+      jsonTree.value = JSON.stringify(jsonTreeObj, null, 2)
+      jsonButtonState.value = jsonParseState.level3;
     } catch (e) {
       console.error(e);
       apiError.value = e?.message || String(e);
+    } finally {
+      jsonLevel3TreeLoading.value = false;
+      jsonLoadingInterval = stopLoadingTimer(jsonLoadingInterval);
+      console.log("三级结构树解析结束")
     }
-    console.log(phrasesMap)
+    // console.log(phrasesMap)
     // jsonTree.value = Array.from(phrasesMap.values()).map(v => JSON.stringify(v,null,2));
+  }
 
-    // 对于类型为"名词短语"|"动词短语"的，把其"值"类似parseToLevel2Tree函数中那样加入一个array，最终对其array呼叫parsePhrases，然后进行类似parseToLevel2Tree的原路插回（不过parsePhrases返回的array的元素取代的是{ "类型": "名词短语"|"动词短语", "值": "<...>" }这个对象）
-    // 对于类型为"句子"的，通过Promise.allSettled进行并发的parseSentence。对返回的结果也类似地呼叫parseConstituents，但解析出来的"句子"类型全部换成"名词短语"。然后就是同样的对"名词短语"|"动词短语"的处理。
-    //二级结构树的一个例子：
-    //[{"语气助词":"","情景列":[{"类型":"句子","值":"如果天黑了"}],"主语":"我","谓语列":[{"谓语":{"类型":"动词短语","值":"回"},"宾语列":[{"类型":"名词短语","值":"镇子里"}],"介词短语列":[{"介词":"工具/手段","介词宾语":{"类型":"名词短语","值":"汽车"}}]}]}]
 
-    jsonLevel3TreeLoading.value = false;
-    jsonLoadingInterval = stopLoadingTimer(jsonLoadingInterval);
-    console.log("三级结构树解析结束")
+  async function translateToTokiPona() {
+
   }
 
   async function handleJsonClick() {
